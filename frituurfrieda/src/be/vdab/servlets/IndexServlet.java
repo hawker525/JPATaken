2b4d42a790745e61000574ff3462cf2151590b15
Maarten Westelinck <maartenwestelinck@hotmail.com>
2016-11-21 14:28:55 +0100

    updated .gitignore

--- frituurfrieda/src/be/vdab/servlets/IndexServlet.java
+++ frituurfrieda/src/be/vdab/servlets/IndexServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet("/index.html")
+//@WebServlet("/index.html")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
