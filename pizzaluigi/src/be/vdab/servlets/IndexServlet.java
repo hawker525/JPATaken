1277717082822d1ffca8242d9a47dcc310bb2b2e
Maarten Westelinck <maartenwestelinck@hotmail.com>
2016-11-21 15:09:26 +0100

    theorie afgewerkt tot 7.4

--- pizzaluigi/src/be/vdab/servlets/IndexServlet.java
+++ pizzaluigi/src/be/vdab/servlets/IndexServlet.java
@@ -12,9 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class IndexServlet
  */
-//@WebServlet("/index.html")
+@WebServlet("/index.html")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
 
     /**
      * Default constructor. 
@@ -27,14 +28,9 @@ public class IndexServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<!doctype html>");
-		out.println("<html lang='nl'><head>");
-		out.println("<title>Pizza Luigi</title></head>");
-		out.println("<body><h1>");
 		int uur = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-		out.print(uur >= 6 && uur < 12 ? "Goede morgen" : uur >= 12 && uur < 18 ? "Goede middag": "Goede avond");
-		out.println("</h1></body></html>");
+		request.setAttribute("begroeting", uur >= 6 && uur < 12 ? "Goede morgen" : uur >= 12 && uur < 18 ? "Goede middag": "Goede avond");
+		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
 }
