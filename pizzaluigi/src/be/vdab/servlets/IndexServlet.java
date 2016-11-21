c78edd45c5db6f2b101fe49597cb7a033fe52a18
Maarten Westelinck <maartenwestelinck@hotmail.com>
2016-11-21 14:24:50 +0100

    theorie t.e.m. 6.7 afgewerkt

--- pizzaluigi/src/be/vdab/servlets/IndexServlet.java
+++ pizzaluigi/src/be/vdab/servlets/IndexServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet("/index.html")
+//@WebServlet("/index.html")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
