3f8bb7d0e26016e9143ed82dfd6153ed4a03a2f9
Maarten Westelinck <maartenwestelinck@hotmail.com>
2016-11-22 15:29:30 +0100

    taak 2.8 afgewerkt

--- frituurfrieda/src/be/vdab/servlets/IndexServlet.java
+++ frituurfrieda/src/be/vdab/servlets/IndexServlet.java
@@ -14,16 +14,23 @@ import be.vdab.entities.Gemeente;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet("/index.html")
+@WebServlet(urlPatterns="/index.html", name="indexservlet")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
+	private final Adres adres = new Adres();
+	private final Gemeente gemeente = new Gemeente();
 
     /**
      * Default constructor. 
+     * @return 
      */
-    public IndexServlet() {
-        // TODO Auto-generated constructor stub
+    public void init() throws ServletException {
+    	adres.setStraat(this.getInitParameter("straat"));
+		adres.setHuisNr(this.getInitParameter("huisnummer"));
+		gemeente.setNaam(this.getInitParameter("gemeente"));
+		gemeente.setPostCode(Integer.parseInt(this.getInitParameter("postcode")));
+		adres.setGemeente(gemeente);
     }
 
 	/**
@@ -31,12 +38,6 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int dag = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-		Adres adres = new Adres();
-		adres.setStraat("testStraat");
-		adres.setHuisNr("1");
-		adres.setGemeente(new Gemeente());
-		adres.getGemeente().setNaam("Wondelgem");
-		adres.getGemeente().setPostCode(9940);
 		String openGesloten = (dag == Calendar.MONDAY || dag == Calendar.THURSDAY) ? "gesloten" : "open";
 
 		request.setAttribute("adres", adres);
